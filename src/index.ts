247ed7677e69e5dbeaab8be16a1ecd40062ce1ba
James11T <jamail090999@gmail.com>
2023-08-19 14:14:57 +0100

    add cors

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import express from "express";
 import assertiveEnv from "./utils/assertiveEnv.js";
 import errorHandler from "./error.js";
 import orderRouter from "./routes/order.js";
+import cors from "cors";
 
 const { PORT } = assertiveEnv("PORT");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/order", orderRouter);
 app.use(errorHandler);
